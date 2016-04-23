42704c2e755eea12c780fc2182a129fee68c9625
dilson-bd <dilson.ti@outlook.com>
2016-04-23 01:53:19 -0300

    Added files via upload

---
+++ select_um.php
@@ -0,0 +1,22 @@
+/*MODEL*/
+public function select_andamentos()
+{           
+  $this->db->distinct();
+  $this->db->select("Andamento");
+  $this->db->order_by("Andamento", "asc");
+  $consulta = $this->db->get("lancamentos");
+  return $consulta;
+}
+
+
+/*CONTROLLER*/	
+$dados['seleciona_andamento'] = $this->m_clientes->select_andamentos();
+
+
+/*VIEW*/
+<select name="andamento" id="andamento" onchange="busca_andamento($(this).val())">
+<option value=""></option>
+<? foreach($seleciona_andamento -> result() as $linha): ?>			
+<option value="<?= isset($tipo) ? $linha->Andamento : '' ?>"><? echo $linha->Andamento; ?></option>	
+<? endforeach; ?>
+</select>	
